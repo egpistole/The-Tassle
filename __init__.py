dc7fc32fd6ee8e3fa0660c1736e51e6a61a60d4e
egpistole <egpistole@gmail.com>
2026-05-28 22:08:00 -0500

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,18 @@
-#
-# This file is part of gunicorn released under the MIT license.
-# See the NOTICE for more information.
+from __future__ import annotations
 
-version_info = (26, 0, 0)
-__version__ = ".".join([str(v) for v in version_info])
-SERVER = "gunicorn"
-SERVER_SOFTWARE = "%s/%s" % (SERVER, __version__)
+from pip._internal.utils import _log
+
+# init_logging() must be called before any call to logging.getLogger()
+# which happens at import of most modules.
+_log.init_logging()
+
+
+def main(args: list[str] | None = None) -> int:
+    """This is preserved for old console scripts that may still be referencing
+    it.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
