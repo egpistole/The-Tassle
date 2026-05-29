5a239818f2dca74e8a798f6e77b21d55655739d3
egpistole <egpistole@gmail.com>
2026-05-28 22:19:04 -0500

    Add files via upload

--- errors.py
+++ errors.py
@@ -1,28 +1,34 @@
-#
-# This file is part of gunicorn released under the MIT license.
-# See the NOTICE for more information.
-
-# We don't need to call super() in __init__ methods of our
-# BaseException and Exception classes because we also define
-# our own __str__ methods so there is no need to pass 'message'
-# to the base class to get a meaningful output from 'str(exc)'.
-# pylint: disable=super-init-not-called
+class ConsoleError(Exception):
+    """An error in console operation."""
 
 
-# we inherit from BaseException here to make sure to not be caught
-# at application level
-class HaltServer(BaseException):
-    def __init__(self, reason, exit_status=1):
-        self.reason = reason
-        self.exit_status = exit_status
-
-    def __str__(self):
-        return "<HaltServer %r %d>" % (self.reason, self.exit_status)
+class StyleError(Exception):
+    """An error in styles."""
 
 
-class ConfigError(Exception):
-    """ Exception raised on config error """
+class StyleSyntaxError(ConsoleError):
+    """Style was badly formatted."""
 
 
-class AppImportError(Exception):
-    """ Exception raised when loading an application """
+class MissingStyle(StyleError):
+    """No such style."""
+
+
+class StyleStackError(ConsoleError):
+    """Style stack is invalid."""
+
+
+class NotRenderableError(ConsoleError):
+    """Object is not renderable."""
+
+
+class MarkupError(ConsoleError):
+    """Markup was badly formatted."""
+
+
+class LiveError(ConsoleError):
+    """Error related to Live display."""
+
+
+class NoAltScreen(ConsoleError):
+    """Alt screen mode was required."""
